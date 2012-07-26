508de6336080675afee8ffa3adcab3fd8bcda3e6
lznt <lasseann@CSE-PC59.(none)>
2012-07-26 15:04:23 +0300

    Bugfixes and some extensions

--- websocketserver/websocketserver.py
+++ websocketserver/websocketserver.py
@@ -44,101 +44,48 @@ def on_sceneadded(name):
     scene = sceneapi.GetScene(name).get() #*Raw
     print "Using scene:", scene.name, scene
 
-    assert scene.connect("AttributeChanged(IComponent*, IAttribute*, AttributeChange::Type)", onAttributeChanged)
-    assert scene.connect("EntityCreated(Entity*, AttributeChange::Type)", onNewEntity)
+    #assert scene.connect("AttributeChanged(IComponent*, IAttribute*, AttributeChange::Type)", onAttributeChanged)
+    #assert scene.connect("EntityCreated(Entity*, AttributeChange::Type)", onNewEntity)
 
-    assert scene.connect("ComponentAdded(Entity*, IComponent*, AttributeChange::Type)", onComponentAdded)
+    #assert scene.connect("ComponentAdded(Entity*, IComponent*, AttributeChange::Type)", onComponentAdded)
 
-    assert scene.connect("EntityRemoved(Entity*, AttributeChange::Type)", onEntityRemoved)
+    #assert scene.connect("EntityRemoved(Entity*, AttributeChange::Type)", onEntityRemoved)
 
     
 def update(t):
 	if server is not None:
 		#server.next()
 		server._stopped_event.wait(timeout=0.0001)
+		#print 'smth'
+		#This functionality makes sure that we send msg's only when a player has been busted. Sends the players name to server.
+		#Server then messages the player for being busted.
+		if GraffitiWebSocket.Ready == True:
+			Logic = tundra.Scene().MainCameraScene().GetEntityByName("Logic").get()
+			if Logic.dynamiccomponent.GetAttribute('Busted') == True:
+				player = Logic.dynamiccomponent.GetAttribute('PlayerName')
+				sendAll({"action" : "busted" , "data":{"user": {"name" : player}}})
+		#Here attanch someday the messagesender to phone.
 		#print '.',
 		#def on_exit(self):
 		# Need to figure something out what to do and how
 		#if walk == True:
-			#moveUpdate(t)
-
-			
+		#moveUpdate(t)
+		
 def sendAll(data):
 	for client in clients:
 		try:
 			client.send(json.dumps(data))
+
 		except socket.error:
 			pass #client has been disconnected, will be noted later & disconnected by another part
 
-def onAttributeChanged(component, attribute, changeType):
-    #FIXME Only syncs hard coded ec_placeable
-    #Maybe get attribute or something
-    
-    #FIXME Find a better way to get component name
-    component_name = str(component).split()[0]
-
-    #Let's only sync EC_Placeable
-    if component_name != "EC_Placeable":
-       return
-
-    entity = component.ParentEntity()
-    
-    # Don't sync local stuff
-    if entity.IsLocal():
-        return
-
-    ent_id = entity.id
-
-    data = component.GetAttributeQVariant('Transform')
-    transform = list()
-
-    transform.extend([data.position().x(), data.position().y(), data.position().z()])
-    transform.extend([data.rotation().x(), data.rotation().y(), data.rotation().z()])
-    transform.extend([data.scale().x(), data.scale().y(), data.scale().z()])
-
-    sendAll(['setAttr', {'id': ent_id, 
-                         'component': component_name,
-                         'Transform': transform}])
-
-def onNewEntity(entity, changeType):
-    sendAll(['addEntity', {'id': entity.id}])
-    print entity
-
-def onComponentAdded(entity, component, changeType):
-    #FIXME Find a better way to get component name
-    component_name = str(component).split()[0]
-
-    # Just sync EC_Placeable and EC_Mesh since they are currently the
-    # only ones that are used in the client
-    if component_name not in ["EC_Placeable", "EC_Mesh"]:
-        return
-
-    if component_name == "EC_Mesh":
-        sendAll(['addComponent', {'id': entity.id, 'component': component_name, 'url': 'ankka.dae'}])
-
-    else: #must be pleaceable
-        data = component.transform
-        transform = list()
-
-        transform.extend([data.position().x(), data.position().y(), data.position().z()])
-        transform.extend([data.rotation().x(), data.rotation().y(), data.rotation().z()])
-        transform.extend([data.scale().x(), data.scale().y(), data.scale().z()])
-
-        sendAll(['addComponent', {'id': entity.id, 
-                             'component': component_name,
-                             'Transform': transform}])
-
-    print entity.id, component
-
-def onEntityRemoved(entity, changeType):
-    print "Removing", entity
-    sendAll(['removeEntity', {'id': entity.id}])
-
 
 class GraffitiWebSocket(WebSocket):
-
+	
 	walk = False
 	avatarEntity = None
+	police = None
+	policeId = 0
 	relativeLat = 0
 	relativeLon = 0
 	totalLat = 0
@@ -149,18 +96,21 @@ class GraffitiWebSocket(WebSocket):
 	ratioLat = 0
 	ratioLon = 0
 	angle = 0
-	PoliceId = 1
+	Ready = False
+	Logic = None
+	Busted = False
 	
 	def opened(self):
 		print "Websocket client connected"
-		self.send('Websocket client connected')
-
+		#self.send('Websocket client connected')
+		clients.add(self)
+		
 	def received_message(self, message):
 		"""
 		Automatically sends back the provided ``message`` to
 		its originating endpoint.
 		"""
-
+		print(message)
 		lat1 = 65.012124
 		lon1 = 25.473395
 		#lat1 = 65.058325
@@ -169,29 +119,32 @@ class GraffitiWebSocket(WebSocket):
 		def add():
 			#In final version visible after the first move.
 			print "Player Added"
-			avatarEntity =  tundra.Scene().MainCameraScene().CreateEntity(scene.NextFreeId(), ["EC_Placeable","EC_DynamicComponent", "EC_AnimationController", "EC_Mesh", "EC_RigidBody", "EC_Avatar", "EC_Script"]).get()
+			avatarEntity =  tundra.Scene().MainCameraScene().CreateEntity(scene.NextFreeId(), ["EC_Placeable","EC_DynamicComponent", "EC_AnimationController", "EC_Mesh", "EC_RigidBody", "EC_Avatar"]).get()
 			avatarEntity.SetTemporary(True)
+			avatarEntity.GetOrCreateComponent("EC_Script", 'Player')
 			avatarEntity.placeable.visible = True
-			avatarEntity.SetName("Bot" + str(msg['data']['user']))
+			avatarEntity.SetName(str(msg['data']['user']['name']))
 			avatarEntity.rigidbody.mass = 0
 			avatarEntity.placeable.SetPosition(0, -4, 0)
 			avatarEntity.dynamiccomponent.CreateAttribute('float3', 'ratios')
 			avatarEntity.dynamiccomponent.CreateAttribute('float3', 'toMoves')
 			avatarEntity.dynamiccomponent.CreateAttribute('float3', 'angleOfOrientation')
 			avatarEntity.dynamiccomponent.CreateAttribute('bool', 'ifToWalk')
-			avatarEntity.dynamiccomponent.CreateAttribute('bool', 'busted')
+			avatarEntity.dynamiccomponent.CreateAttribute('bool', 'Busted')
 			avatarEntity.dynamiccomponent.CreateAttribute('float3', 'totals')
 			avatarEntity.dynamiccomponent.CreateAttribute('bool', 'reset')
 			avatarEntity.dynamiccomponent.CreateAttribute('bool', 'Spraying')
+			avatarEntity.dynamiccomponent.SetAttribute('Spraying', False)
 			#Need to add some value to check players team and set it after that.
 			avatarEntity.dynamiccomponent.CreateAttribute('string', 'Team')
 			avatarEntity.dynamiccomponent.CreateAttribute('string', 'Role')
+			avatarEntity.dynamiccomponent.CreateAttribute('bool', 'rdyToSpray')
 			avatarEntity.dynamiccomponent.SetAttribute('Role', 'Player')
 			#avatarEntity.avatar.appearanceRef.setRef("default_avatar.avatar")
 			avatarEntity.script.className = "BotScriptApp.BotScript"
 			#C2.className = "BotAndPoliceApp.BotAndPolice"
-			avatarEntity.dynamiccomponent.SetAttribute('Team', str(msg['data']['team']))
-			
+			avatarEntity.dynamiccomponent.SetAttribute('Team', str(msg['data']['user']['team']))
+			GraffitiWebSocket.Ready = True
 			#Approx 0 on oulu3d
 			#long = 25.473395
 			#lat = 65.012124
@@ -201,21 +154,20 @@ class GraffitiWebSocket(WebSocket):
 			policeEntity = tundra.Scene().MainCameraScene().CreateEntity(scene.NextFreeId(),["EC_Placeable", "EC_DynamicComponent", "EC_AnimationController", "EC_Mesh", "EC_RigidBody", "EC_Avatar", "EC_Script"]).get()
 			policeEntity.SetTemporary(True)
 			policeEntity.placeable.visible = False
-			policeEntity.SetName("Bot_Police")
+			policeEntity.SetName("Police")
 			#GraffitiWebSocket.PoliceId + 1
 			policeEntity.rigidbody.mass = 0
 			#policeEntity.placeable.SetPosition(0, -4, 0)
-			policeEntity.dynamiccomponent.CreateAttribute('string', 'Role')
-			policeEntity.dynamiccomponent.SetAttribute('Role', 'Police')
+			policeEntity.dynamiccomponent.CreateAttribute('bool', 'Spraying')
+			Logic = tundra.Scene().MainCameraScene().GetEntityByName('Logic')
 			policeEntity.script.className = "PoliceScriptApp.PoliceScript"
 
 			
 		def move():
 			
-			avatarEntity = tundra.Scene().MainCameraScene().GetEntityByName("Bot" + str(msg['data']['_id'])).get()
-			length = len(msg['data']['localizations'])
-			lat =  msg['data']['localizations'][length-1]['lat']
-			lon =  msg['data']['localizations'][length-1]['lon']
+			avatarEntity = tundra.Scene().MainCameraScene().GetEntityByName(str(msg['data']['user']['name'])).get()
+			lat =  msg['data']['localization']['lat']
+			lon =  msg['data']['localization']['lon']
 			#print lat, lon
 			#print lat1, lon1
 			#lat = msg['data']['lat']
@@ -283,41 +235,66 @@ class GraffitiWebSocket(WebSocket):
 			
 		
 		def spray():
-			#Need to add some id for screens, to identify at this stage that which screen is being used.
-			#Currently manipulating always 'screen', in future we can change all this by some identification.
-			avatarEntity = tundra.Scene().MainCameraScene().GetEntityByName("Bot" + str(msg['data']['_id'])).get()
+			#Get spraying player, sprayed venue and particleEffect for that venue.
+			avatarEntity = tundra.Scene().MainCameraScene().GetEntityByName(str(msg['data']['user']['name'])).get()
+			screen = tundra.Scene().MainCameraScene().GetEntityByName(str(msg['data']['venue']['name'])).get()
+			particle = tundra.Scene().MainCameraScene().GetEntityByName(str(msg['data']['venue']['name']) + "spray").get()
+			Logic = tundra.Scene().MainCameraScene().GetEntityByName('Logic').get()
+			id = Logic.dynamiccomponent.GetAttribute('id')
+			
+			particleName = (str(msg['data']['venue']['name']) + "spray")
+			screenName = str(msg['data']['venue']['name'])
+			team = avatarEntity.dynamiccomponent.GetAttribute('Team')
+			playerPos = screen.dynamiccomponent.GetAttribute('playerPos')
+			particlePos = avatarEntity.dynamiccomponent.GetAttribute('particlePos')
+			#Set player current position, to use them to calculate distance from area.
+			curPosx = avatarEntity.placeable.Position().x()
+			curPosz = avatarEntity.placeable.Position().z()
+			playerPos.setx(curPosx)
+			playerPos.setz(curPosz)
+			#Position of screen, to move player to spray if spraying in that area.
+			particlePosx = particle.placeable.Position().x()
+			particlePosz = particle.placeable.Position().z()
+			particlePos.setx(particlePosx)
+			particlePos.setz(particlePosz)
+			#Set all needed information for dynamiccomponents, so that scripts can use them.
+			screen.dynamiccomponent.SetAttribute('screenName', str(msg['data']['venue']['name']))
+			screen.dynamiccomponent.SetAttribute('playerPos', playerPos)
+			screen.dynamiccomponent.SetAttribute('PlayerId', str(msg['data']['user']['name']))
+			screen.dynamiccomponent.SetAttribute('PlayerTeam' , team)
+			screen.dynamiccomponent.SetAttribute('PlayerName', str(msg['data']['user']['name']))
+			screen.dynamiccomponent.SetAttribute('Spraying', True)
+			particle.dynamiccomponent.SetAttribute('particleName', str(msg['data']['venue']['name']) + "spray")
+			particle.dynamiccomponent.SetAttribute('PlayerName', str(msg['data']['user']['name']))
+			particle.dynamiccomponent.SetAttribute('Spraying', True)
+			avatarEntity.dynamiccomponent.SetAttribute('particlePos', particlePos)
 			avatarEntity.dynamiccomponent.SetAttribute('Spraying', True)
-			name = msg['data']['_id']
-			#The lousy variable to decide the destiny of our screen. Currently can only use 1 screen, since we insert stuff to screen by name
-		
-			#In here we get the msg from server, that tells which team has painted, and sends the variable to
-			#dynamiccomponent where its fetched by TeamMaterials.js and then used.
-		
-		
+			avatarEntity.dynamiccomponent.SetAttribute('screenName', str(msg['data']['venue']['name']))
+			
+			
 	
 		#print "Websocket message received:"
 		
 		#print message.data.decode("utf-8")
 			
 		msg = json.loads(message.data.decode("utf-8"))    
-		self.send(message.data, message.is_binary)
-	
+		#self.send(message.data, message.is_binary)
+		
 
 			
 			
-		actions = { "addMobileUser" : add, "movedUser" : move, "sprayGraffiti" : spray, "addPolice" : addPolice}
+		actions = { "add" : add, "move" : move, "spray" : spray, "addPolice" : addPolice}
 
 		actions[msg['action']]()      
 
-			
-
+	
 if tundra.Server().IsAboutToStart():
 	server = ws4py.server.geventserver.WebSocketServer(('0.0.0.0', 9999), websocket_class=GraffitiWebSocket)
 	server.start()
 	print "websocket server started."
 	assert tundra.Frame().connect("Updated(float)", update)
 
-	                             
+	                            
 	sceneapi = tundra.Scene()
 	
 	print "Websocket Server connecting to OnSceneAdded:", sceneapi.connect("SceneAdded(QString)", on_sceneadded)
